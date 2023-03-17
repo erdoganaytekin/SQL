3c6410c3abdffd0630bd6955c754c04587633b07
erdoganaytekin <115734800+erdoganaytekin@users.noreply.github.com>
2023-03-18 00:40:58 +0300

    Update RDB&SQL Assignment-3.sql

--- RDB&SQL Assignment-3.sql
+++ RDB&SQL Assignment-3.sql
@@ -1,6 +1,6 @@
 --- RDB&SQL Assignment-3 (DS 13/22 EU)
 
-/* Discount Effects 
+/* Discount Effects
 
 Generate a report including product IDs and discount effects on whether the increase in the discount rate positively impacts the number of orders for the products.
 
